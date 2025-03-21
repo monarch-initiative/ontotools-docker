3da9cc98a39c005b7ce256c0b29150712d6e0e21
Nico Matentzoglu <nicolas.matentzoglu@gmail.com>
2025-03-22 00:40:52 +0200

    Automate the entire pipeline

--- runols4.sh
+++ runols4.sh
@@ -1,5 +1,6 @@
 export OLS4_CONFIG=./config/ols-config/ols-config.json
+
 mkdir -p tmp
-#docker compose rm -f -v
-#docker compose up ols4-dataload --force-recreate
-docker compose up --force-recreate
+sh odk.sh make ontologies -B
+docker compose up ols4-dataload --force-recreate
+docker compose up ols4-frontend
